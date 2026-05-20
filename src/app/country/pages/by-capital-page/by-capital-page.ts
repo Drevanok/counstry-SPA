07cab58b05aaaf5edbca2b0d92130728275b207d
Drevanok <sauleliseoaparicio@gmail.com>
2026-05-19 22:52:28 -0700

    feat: create top-menu-component and add content by-capital-page

--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
   templateUrl: './by-capital-page.html',
 })
-export class ByCapitalPage {}
+export class ByCapitalPage {
+  onSearch(value: string) {
+    console.log(value)
+  }
+}
