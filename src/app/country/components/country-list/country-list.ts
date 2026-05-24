82153ac98ccb8afb01aa9cb424ca940460a64fbf
Drevanok <sauleliseoaparicio@gmail.com>
2026-05-23 20:26:10 -0700

    feat: add service countries

--- src/app/country/components/country-list/country-list.ts
+++ src/app/country/components/country-list/country-list.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
 
 @Component({
   selector: 'country-list',
@@ -6,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './country-list.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryList {}
+export class CountryList {
+  countries = input.required<RESTCountry[]>()
+}
--- src/app/country/pages/by-capital-page/by-capital-page.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.ts
@@ -1,17 +1,33 @@
-import { Component, inject } from '@angular/core';
-import { CountrySearchInput } from "../../components/search-input/search-input";
+import { Component, inject, signal } from '@angular/core';
+import { CountrySearchInput } from '../../components/search-input/search-input';
 import { CountryList } from "../../components/country-list/country-list";
-import { Router } from '@angular/router';
+import { CountryService } from '../../services/country';
+import { RESTCountry } from '../../interfaces/rest-countries.interface';
+
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.html',
   imports: [CountrySearchInput, CountryList],
 })
 export class ByCapitalPage {
-  private router = inject(Router);
-  
-  onSearch(value: string) {
-    this.router.navigate(['country/by', value]);
+  CountryService = inject(CountryService)
+
+  isLoading = signal(false);
+  hasError = signal<string | null>(null);
+  countries = signal<RESTCountry[]>([]);
+
+  onSearch(query: string) {
+    if (this.isLoading()) return
+
+    this.isLoading.set(true);
+    this.hasError.set(null);
+
+    this.CountryService.searchByCapital(query).subscribe((countries) => {
+      this.isLoading.set(false);
+      this.countries.set(countries)
+      
+      console.log(countries)
+    });
   }
 
 }
