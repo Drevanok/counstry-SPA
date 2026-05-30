1ae9af0552d78bc468b1a85b2f66e6819530ed11
Drevanok <sauleliseoaparicio@gmail.com>
2026-05-29 21:16:27 -0700

    feat: add content country-information page

--- src/app/country/interfaces/country.interface.ts
+++ src/app/country/interfaces/country.interface.ts
@@ -1,3 +1,4 @@
+import { ByRegionPage } from '../pages/by-region-page/by-region-page';
 export interface Country {
     cca2: string;
     flag: string;
@@ -5,4 +6,7 @@ export interface Country {
     name: string;
     capital: string;
     population: number;
+
+    region: string;
+    subRegion: string;
 }
